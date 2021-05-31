0fba8312f2acd1b4ca0147e557a42a2d57ae1cea
Javi <javiersmr471@gmail.com>
2021-05-31 10:58:01 +0200

    Credito a medias

--- ejercicio1/src/tests/CreditoTest.java
+++ ejercicio1/src/tests/CreditoTest.java
@@ -2,28 +2,54 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import dominio.Credito;
+
 class CreditoTest {
+	private Credito c;
+	
+	@BeforeEach
+	public void init(){
+		Credito c=new Credito("2313","Paco",null,1000);
+	}
+	@AfterEach
+	public void finish(){
+		c=null;
+	}
 
 	@Test
-	void testRetirar() {
-		fail("Not yet implemented");
+	void testRetirar() throws Exception {
+		c.retirar(100);
+		assertEquals(900, c.getCreditoDisponible());
 	}
 
 	@Test
 	void testIngresar() {
-		fail("Not yet implemented");
+		try {
+			c.ingresar(100);
+			assertEquals(1100,c.getCreditoDisponible());
+		}catch(Exception e) {
+			
+		}
 	}
 
 	@Test
 	void testPagoEnEstablecimiento() {
-		fail("Not yet implemented");
+		try {
+			c.pagoEnEstablecimiento("datos", 100);
+			assertEquals(900,c.getCreditoDisponible());
+		}catch(Exception e) {
+			
+		}
 	}
 
 	@Test
 	void testGetCreditoDisponible() {
-		fail("Not yet implemented");
+
+		assertEquals(1000, c.getSaldo());
 	}
 
 	@Test
